74fe229d033b81998acb43d6f48ca0f6d8e2539d
Luis Trejo <134348738+Luistrejo8877@users.noreply.github.com>
2023-10-04 20:57:57 -0700

    chat-api

---
+++ readme.md
@@ -0,0 +1,3 @@
+1.- Common JS 
+2.- database.js
+3.- Sequelize CLI - Command Line Interface 
---
+++ src/routes/error.routes.js
@@ -0,0 +1,17 @@
+const {
+  errorLogger,
+  ormErrorHandler,
+  errorHandler,
+  notFoundErrorHandler,
+} = require("../middlewares/errors.middleware");
+
+const errorRoutes = (app) => {
+  app.use(errorLogger);
+  app.use(ormErrorHandler);
+  app.use(errorHandler);
+  app.use(notFoundErrorHandler);
+};
+
+module.exports = errorRoutes;
+
+  
